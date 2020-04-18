9f1bcfa254faa0ecbdc8629cbdd0fdd1ab2ec87c
Sande <Sande@DESKTOP-A7FGHVE.attlocal.net>
2020-04-17 20:18:51 -0400

    Bye.java file updated

--- src/com/src/E2EIntegration/Bye.java
+++ src/com/src/E2EIntegration/Bye.java
@@ -2,9 +2,11 @@ package com.src.E2EIntegration;
 
 public class Bye {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 
+	System.out.println("Inside E2E Integration");
 	}
 
 }
